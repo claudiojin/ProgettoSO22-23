99d3b9eb4d3861a7aaaf9a8bcde00942387389ce
braghira <andrea.venturoli5@studio.unibo.it>
2023-02-16 21:58:44 +0100

    Test Proof, freeNamespace not implemented

---
+++ ns.h
@@ -0,0 +1,9 @@
+#include "ash.h"
+#include "pcb.h"
+
+void init_nsFree(nsd_t table[], nsd_t *free_list, nsd_t *active_list);
+void initNamespaces();
+nsd_t *getNamespace(pcb_t *p, int type);
+int addNamespace(pcb_t *p, nsd_t *ns);
+nsd_t *allocNamespace(int type);
+void freeNamespace(nsd_t *ns);
