c043b52fea90a1ef6fe23fa668c0bf3d1154cb7a
Claudio Jin <claudio.jin@studio.unibo.it>
2023-09-05 10:40:54 +0200

    scheduler

--- fase2/initial.c
+++ fase2/initial.c
@@ -1,36 +1,9 @@
-#include "../headers/pandos_types.h"
-#include "../headers/pandos_const.h"
-#include "../headers/exceptions.h"
-#include "../headers/scheduler.h"
-#include "../headers/pcb.h"
-#include "../headers/ash.h"
-#include "../headers/ns.h"
-#include <umps3/umps/cp0.h>
-#include <umps3/umps/libumps.h>
-
+#include "../headers/initial.h"
 /**
  * This module implements main() and exports the Nucleus’s global variables.
  * (e.g. process count, device semaphores, etc.)
  */
 
-// LEVEL 3 GLOBAL VARIABLES
-
-// integer indicating the number of started, but not yet terminated processes
-int process_count;
-// process can be either in the “ready,” “running,” or “blocked” (also known as “waiting”) state. This integer is
-// the number of started, but not terminated processes that in are the “blocked” state due to an I/O or timer request
-int softBlock_count;
-// Tail pointer to a queue of pcbs that are in the “ready” state
-struct list_head ready_queue;
-// pointer to the pcb that is in the “running” state, i.e. the current executing process
-pcb_t* curr_process;
-// The Nucleus maintains one integer semaphore for each external (sub)device in μMPS3, plus one additional semaphore
-// to support the Pseudo-clock. Since terminal devices are actually two independent sub-devices, the Nucleus
-// maintains two semaphores for each terminal device
-int device_semaphores[DEV_SEMAPHORES];
-// pointer to process 0 pass up vector memory location
-passupvector_t* passupvector;
-state_t process0_state;
 
 // functions provided by the p2test
 extern void test();
--- fase2/scheduler.c
+++ fase2/scheduler.c
@@ -1,5 +1,51 @@
 #include "../headers/scheduler.h"
+#include "../headers/pcb.h"
+#include "../headers/initial.h"
+#include <umps3/umps/libumps.h>
+#include "../headers/ash.h"
+
 /**
  * This module implements the Scheduler and the deadlock detector.
-*/
-void scheduler() {PANIC();}
+ */
+
+// Selezionare il prossimo processo da mettere in esecuzione
+static pcb_t *getNextProcess()
+{
+    pcb_t *next_proc = NULL;
+    next_proc = removeProcQ(&ready_queue);
+    
+    return next_proc;
+}
+void scheduler()
+{
+    // terminato con successo
+    if (process_count == 0)
+    {
+        HALT();
+    }
+
+    // esegue in modalita' FIFO
+    curr_process = getNextProcess();
+
+    if (curr_process == NULL){
+        // se non la cosa ready_queue e' vuota e softBlock_count > 0,
+        //il processore va in attesa
+        if (softBlock_count > 0){
+            //enable interrupts e termina PLT
+            setSTATUS((getSTATUS() | IECON | IMON) & ~TEBITON);
+            WAIT();
+        }
+        else{
+            PANIC();
+        }
+    }
+    //esiste almeno un elemento nella ready_queue
+    else{
+        curr_process->p_s.status =  (curr_process->p_s.status) | TEBITON;
+        //load PLT
+        setTIMER((cpu_t) TIMESLICE * (*((cpu_t *)TIMESCALEADDR)));
+        
+        LDST(&curr_process->p_s);
+
+    }
+}
